14219f54b616a7a03eff14e7715697d2327bffb2
preffols <princesmnsusa@gmail.com>
2024-03-13 12:10:29 +0200

    "db connection"

--- conn.php
+++ conn.php
@@ -1,15 +1,16 @@
 <?php
 
 
-
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mzuniadmin";
+$username = "u953672825_user";
+$password = "1347Happy";
+$dbname = "u953672825_mzuniadmin";
+
+
 
 try {
- $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
- $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
- die("Database connection failed: " . $e->getMessage());
+    die("Database connection failed: " . $e->getMessage());
 }
